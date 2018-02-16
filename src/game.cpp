6c91e83534303813e970dfbf660bee207f13891d
Borin Ouch <borin.ouch@gmail.com>
2018-02-15 18:38:52 -0800

    Load texture, shaders, and draw sprite

--- src/game.cpp
+++ src/game.cpp
@@ -1,4 +1,8 @@
 #include "game.h"
+#include "resource_manager.h"
+#include "sprite_renderer.h"
+
+SpriteRenderer *renderer;
 
 Game::Game(GLuint width_, GLuint height_)
     : state {GAME_ACTIVE}, keys {}, width {width_}, height {height_}
@@ -9,6 +13,16 @@ Game::~Game() {
 }
 
 void Game::init() {
+    ResourceManager::loadShader("res/shaders/sprite_vs.glsl", "res/shaders/sprite_fs.glsl", nullptr, "sprite");
+
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(width), static_cast<GLfloat>(height), 0.0f, -1.0f, 1.0f);
+
+    ResourceManager::getShader("sprite").use().setInteger("image", 0);
+    ResourceManager::getShader("sprite").use().setMatrix4("projection", projection);
+
+    renderer = new SpriteRenderer(ResourceManager::getShader("sprite"));
+
+    ResourceManager::loadTexture("res/textures/awesomeface.png", GL_TRUE, "face");
 }
 
 void Game::processInput(GLfloat dt) {
@@ -18,5 +32,6 @@ void Game::update(GLfloat dt) {
 }
 
 void Game::render() {
+    renderer->drawSprite(ResourceManager::getTexture("face"), glm::vec2(200, 200), glm::vec2(300, 400), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
 
