465c04f200d02b62c91579744ef86b5e868f32b7
Borin Ouch <borin.ouch@gmail.com>
2018-02-15 18:33:30 -0800

    Fix texture id generation

--- src/texture.cpp
+++ src/texture.cpp
@@ -5,7 +5,10 @@ Texture2D::Texture2D()
     : width {0}, height {0},
       internal_format {GL_RGB}, image_format {GL_RGB},
       wrap_s {GL_REPEAT}, wrap_t {GL_REPEAT},
-      filter_min {GL_LINEAR}, filter_max {GL_LINEAR} { }
+      filter_min {GL_LINEAR}, filter_max {GL_LINEAR}
+{
+    GL_CHECK(glGenTextures(1, &id));
+}
 
 Texture2D::~Texture2D() {
 }
@@ -14,7 +17,7 @@ void Texture2D::generate(GLuint width_, GLuint height_, unsigned char *data) {
     width = width_;
     height = height_;
 
-    GL_CHECK(glBindTexture(GL_TEXTURE_2D, this->id));
+    GL_CHECK(glBindTexture(GL_TEXTURE_2D, id));
     GL_CHECK(glTexImage2D(GL_TEXTURE_2D, 0, internal_format, width, height, 0, image_format, GL_UNSIGNED_BYTE, data));
     GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap_s));
     GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap_t));
@@ -24,6 +27,6 @@ void Texture2D::generate(GLuint width_, GLuint height_, unsigned char *data) {
 }
 
 void Texture2D::bind() const {
-    GL_CHECK(glBindTexture(GL_TEXTURE_2D, this->id));
+    GL_CHECK(glBindTexture(GL_TEXTURE_2D, id));
 }
 
